97463295ce0fa6a66389ecb232507e0c5754821f
nairahov87@gmil.com <nairahov@gmail.com>
2023-07-07 14:48:19 +0400

    created tab logic in index.js, and addited css file

---
+++ js/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", function () {
+// tab logic start
+    const tabsHeaders = document.querySelectorAll(".tabheader__item");
+    const tabsContents = document.querySelectorAll(".tabcontent");
+    const tabsHeadersParent = document.querySelector(".tabheader__items");
+    
+ function hideTabContent () {
+    tabsContents.forEach(tabContent => {
+    tabContent.classList.add("hide");
+    tabContent.classList.remove("show", "fade");
+    });
+    
+    tabsHeaders.forEach(tabHeader => tabHeader.classList.remove("tabheader__item_active"));
+    }
+    
+    function showTabContent(i = 1) {
+        tabsContents[i].classList.add("show", "fade");
+        tabsContents[i].classList.remove("hide");
+        tabsHeaders[i].classList.add("tabheader__item_active");
+    }
+    
+    hideTabContent();
+    showTabContent();
+    
+    tabsHeadersParent.addEventListener("click", (e) => {
+        if(e.target && e.target.matches(".tabheader__item")) {
+        tabsHeaders.forEach((tabHeader, index) => {
+        if (e.target === tabHeader) {
+            hideTabContent();
+            showTabContent(index);
+            }
+        });
+     }
+}); 
+// tab logic end
+});
+
